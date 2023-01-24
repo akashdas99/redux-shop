3e99743097659bd3971bfbc327efed131223aa4e
Akash <akashdas0599@example.com>
2023-01-24 19:51:18 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetail from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
